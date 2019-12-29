4a6dbd4e5933dca4927fe03dcc21ad37b577b776
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2019-12-29 21:52:07 +0100

    setup: change realm from Fira to fira; add fira-backend

--- keycloak/resources/start.sh
+++ keycloak/resources/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# add admin user for realm Fira
-/opt/jboss/keycloak/bin/add-user-keycloak.sh -r Fira -u ${KEYCLOAK_USER} -p ${KEYCLOAK_PASSWORD}
+# add admin user for realm fira
+/opt/jboss/keycloak/bin/add-user-keycloak.sh -r fira -u ${KEYCLOAK_USER} -p ${KEYCLOAK_PASSWORD}
 # start the server
 /opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0
