08f7bc9c5ea2316def3178a2fc61985ff737ef54
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-01-05 10:28:52 +0100

    feat(auth, fe): implement retry for refresh

--- fira-web/src/http/http.client.ts
+++ fira-web/src/http/http.client.ts
@@ -3,11 +3,6 @@ import axios from 'axios';
 import { HttpException } from './http.exception';
 import { createLogger } from '../logger/logger';
 
-const axiosClient = axios.create({
-  timeout: 5000,
-});
-const logger = createLogger('http.client');
-
 interface LoginRequestDto {
   readonly username: string;
   readonly password: string;
@@ -21,6 +16,13 @@ export interface AuthResponseDto {
   readonly accessToken: string;
   readonly refreshToken: string;
 }
+
+const axiosClient = axios.create({
+  timeout: 5000,
+});
+const logger = createLogger('http.client');
+const REFRESH_RETRY_COUNT = 5;
+const REFRESH_RETRY_DELAY = 3 * 1000; // 3 seconds
 
 export const httpClient = {
   login: async (loginRequest: LoginRequestDto): Promise<AuthResponseDto> => {
@@ -40,11 +42,31 @@ export const httpClient = {
   refresh: async (refreshRequest: RefreshRequestDto): Promise<AuthResponseDto> => {
     logger.info('executing refresh...', { refreshRequest });
 
-    try {
-      return (await axiosClient.post<AuthResponseDto>('auth/v1/refresh', refreshRequest)).data;
-    } catch (e) {
-      logger.error('refresh failed!', e);
-      throw e;
+    // retry refresh with same token 3 times because if the internet connection was idle,
+    // ERR_NETWORK_IO_SUSPENDED can occur on the first try
+    let attempt = 1;
+    let lastError;
+    while (attempt <= REFRESH_RETRY_COUNT) {
+      try {
+        return (await axiosClient.post<AuthResponseDto>('auth/v1/refresh', refreshRequest)).data;
+      } catch (e) {
+        logger.info(`refresh failed for attempt=${attempt}`, { error: e });
+        lastError = e;
+        attempt++;
+        if (e.response?.status) {
+          // got some response from backend --> no retry necessary
+          break;
+        }
+        if (attempt <= REFRESH_RETRY_COUNT) {
+          await timeout(REFRESH_RETRY_DELAY);
+        }
+      }
     }
+    logger.error('refresh failed!', lastError);
+    throw lastError;
   },
 };
+
+function timeout(ms: number) {
+  return new Promise(res => setTimeout(res, ms));
+}
