a24ca0d309d1bace8186e1a118ee9c2f72638733
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-02-10 15:26:58 -0600

    deployment: add web app to backend as static resource

--- fira-be/src/app.module.ts
+++ fira-be/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module, HttpModule, HttpService, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AxiosInstance } from 'axios';
 import nanoid = require('nanoid');
+import * as path from 'path';
 
 import { LoggerModule } from './logger/app-logger.module';
 import { AppLogger } from './logger/app-logger.service';
@@ -20,6 +22,10 @@ import * as config from './config';
   imports: [
     HttpModule,
     LoggerModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'client', 'build'),
+      renderPath: '/',
+    }),
     TypeOrmModule.forRoot({
       ...config.database,
       entities: [
@@ -41,10 +47,7 @@ import * as config from './config';
   providers: [],
 })
 export class AppModule implements OnModuleInit {
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly appLogger: AppLogger,
-  ) {
+  constructor(private readonly httpService: HttpService, private readonly appLogger: AppLogger) {
     this.appLogger.setContext('AppModule');
   }
 
@@ -53,23 +56,14 @@ export class AppModule implements OnModuleInit {
   }
 }
 
-function registerOutgoingHttpInterceptor(
-  axiosInstance: AxiosInstance,
-  appLogger: AppLogger,
-) {
+function registerOutgoingHttpInterceptor(axiosInstance: AxiosInstance, appLogger: AppLogger) {
   axiosInstance.interceptors.request.use(request => {
     try {
       const requestId = nanoid();
       (request as any).requestId = requestId;
-      appLogger.log(
-        `[REQUEST] [${requestId}] ${request.method?.toUpperCase()} ${
-          request.url
-        }`,
-      );
+      appLogger.log(`[REQUEST] [${requestId}] ${request.method?.toUpperCase()} ${request.url}`);
       if (request.data) {
-        appLogger.debug(
-          `[REQUEST PAYLOAD] [${requestId}] ${JSON.stringify(request.data)}`,
-        );
+        appLogger.debug(`[REQUEST PAYLOAD] [${requestId}] ${JSON.stringify(request.data)}`);
       }
     } catch {
       // ignore
@@ -83,9 +77,7 @@ function registerOutgoingHttpInterceptor(
       const requestId = (response.config as any).requestId;
       appLogger.log(`[RESPONSE] [${requestId}] ${response.status}`);
       if (response.data) {
-        appLogger.debug(
-          `[RESPONSE PAYLOAD] [${requestId}] ${JSON.stringify(response.data)}`,
-        );
+        appLogger.debug(`[RESPONSE PAYLOAD] [${requestId}] ${JSON.stringify(response.data)}`);
       }
     } catch {
       // ignore
