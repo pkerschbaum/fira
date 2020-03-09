cd8154719d25f15a60e82dcc66ca23d568c2d018
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-03-09 17:52:22 -0500

    style: improve some styling

--- fira-web/src/typings/enums.ts
+++ fira-web/src/typings/enums.ts
@@ -31,34 +31,28 @@ export enum UserRole {
 }
 
 export type RateLevelType = {
-  readonly text: string;
   readonly relevanceLevel: RelevanceLevel;
   readonly annotationRequired: boolean;
 };
 
 export const RateLevels: RateLevelType[] = [
   {
-    text: 'Misleading Answer',
     relevanceLevel: RelevanceLevel.MISLEADING_ANSWER,
     annotationRequired: false,
   },
   {
-    text: 'Not Relevant',
     relevanceLevel: RelevanceLevel.NOT_RELEVANT,
     annotationRequired: false,
   },
   {
-    text: 'Topic Relevant, But Does Not Answer',
     relevanceLevel: RelevanceLevel.TOPIC_RELEVANT_DOES_NOT_ANSWER,
     annotationRequired: true,
   },
   {
-    text: 'Good Answer',
     relevanceLevel: RelevanceLevel.GOOD_ANSWER,
     annotationRequired: true,
   },
   {
-    text: 'Perfect Answer',
     relevanceLevel: RelevanceLevel.PERFECT_ANSWER,
     annotationRequired: true,
   },
--- fira-web/src/ui/annotation-route/annotate-page/RateButton.tsx
+++ fira-web/src/ui/annotation-route/annotate-page/RateButton.tsx
@@ -4,12 +4,28 @@ import styles from './RateButton.module.css';
 import Button from '../../elements/Button';
 import { RateLevelType, RelevanceLevel } from '../../../typings/enums';
 
-const RATE_LEVEL_STYLES = {
-  [RelevanceLevel.MISLEADING_ANSWER]: styles.misleadingAnswer,
-  [RelevanceLevel.NOT_RELEVANT]: styles.notRelevant,
-  [RelevanceLevel.TOPIC_RELEVANT_DOES_NOT_ANSWER]: styles.topicRelevantDoesNotAnswer,
-  [RelevanceLevel.GOOD_ANSWER]: styles.goodAnswer,
-  [RelevanceLevel.PERFECT_ANSWER]: styles.perfectAnswer,
+const RATE_LEVEL_DATA = {
+  [RelevanceLevel.MISLEADING_ANSWER]: {
+    style: styles.misleadingAnswer,
+    text: 'Misleading Answer',
+    keyboardKey: '1',
+  },
+  [RelevanceLevel.NOT_RELEVANT]: {
+    style: styles.notRelevant,
+    text: 'Not Relevant',
+    keyboardKey: '2',
+  },
+  [RelevanceLevel.TOPIC_RELEVANT_DOES_NOT_ANSWER]: {
+    style: styles.topicRelevantDoesNotAnswer,
+    text: 'Topic Relevant, But Does Not Answer',
+    keyboardKey: '3',
+  },
+  [RelevanceLevel.GOOD_ANSWER]: { style: styles.goodAnswer, text: 'Good Answer', keyboardKey: '4' },
+  [RelevanceLevel.PERFECT_ANSWER]: {
+    style: styles.perfectAnswer,
+    text: 'Perfect Answer',
+    keyboardKey: '5',
+  },
 } as const;
 
 const RateButton: React.FC<{ rateLevel: RateLevelType; onClick: () => void }> = ({
@@ -19,10 +35,11 @@ const RateButton: React.FC<{ rateLevel: RateLevelType; onClick: () => void }> = 
   return (
     <div className={styles.container}>
       <Button
-        className={`${styles.button} ${RATE_LEVEL_STYLES[rateLevel.relevanceLevel]}`}
+        className={`${styles.button} ${RATE_LEVEL_DATA[rateLevel.relevanceLevel].style}`}
         onClick={onClick}
       >
-        {rateLevel.text}
+        <div className={styles.hotkey}>{RATE_LEVEL_DATA[rateLevel.relevanceLevel].keyboardKey}</div>
+        <span>{RATE_LEVEL_DATA[rateLevel.relevanceLevel].text}</span>
       </Button>
     </div>
   );
