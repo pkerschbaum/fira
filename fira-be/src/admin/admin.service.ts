9df57b9220c079d17e8efe463c17b03a9d455932
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-04-03 16:29:46 +0200

    feat(assets): restrict number of parallel imports

--- fira-be/src/admin/admin.service.ts
+++ fira-be/src/admin/admin.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, Connection } from 'typeorm';
 
+import * as config from '../config';
 import { PersistenceService } from '../persistence/persistence.service';
 import {
   ImportAsset,
@@ -15,8 +16,10 @@ import { Document, DocumentVersion } from './entity/document.entity';
 import { Query, QueryVersion } from './entity/query.entity';
 import { JudgementPair } from './entity/judgement-pair.entity';
 import { Config } from './entity/config.entity';
-import * as config from '../config';
 import { assetUtil } from './asset.util';
+import { partitionArray, flatten } from 'src/util/arrays';
+
+const NUMBER_PARALLEL_IMPORTS = 10;
 
 @Injectable()
 export class AdminService {
@@ -28,79 +31,105 @@ export class AdminService {
   ) {}
 
   public importDocuments = this.persistenceService.wrapInTransaction(
-    (transactionalEntityManager, documents: ImportAsset[]): Promise<ImportResult[]> => {
-      return Promise.all(
-        documents.map(async (document) => {
-          try {
-            let dbDocument = await transactionalEntityManager.findOne(Document, document.id);
-            if (!dbDocument) {
-              dbDocument = new Document();
-              dbDocument.id = document.id;
+    async (transactionalEntityManager, documents: ImportAsset[]): Promise<ImportResult[]> => {
+      // create partitions which are processed in parallel
+      const partitions = partitionArray(documents, NUMBER_PARALLEL_IMPORTS);
+
+      // process partitions
+      const results = await Promise.all(
+        partitions.map(async (partition) => {
+          const partitionResults: ImportResult[] = [];
+
+          for (const document of partition) {
+            try {
+              let dbDocument = await transactionalEntityManager.findOne(Document, document.id);
+              if (!dbDocument) {
+                dbDocument = new Document();
+                dbDocument.id = document.id;
+              }
+
+              const maxVersionNumber = await assetUtil.findMaxDocumentVersion(
+                dbDocument,
+                transactionalEntityManager,
+              );
+
+              const dbEntry = new DocumentVersion();
+              dbEntry.document = dbDocument;
+              dbEntry.text = document.text;
+              dbEntry.annotateParts = document.text
+                .split(config.application.splitRegex)
+                .filter((part) => part !== '');
+              if (maxVersionNumber !== undefined && maxVersionNumber !== null) {
+                dbEntry.version = maxVersionNumber + 1;
+              }
+
+              await transactionalEntityManager.save(DocumentVersion, dbEntry);
+              partitionResults.push({ id: document.id, status: ImportStatus.SUCCESS });
+            } catch (e) {
+              partitionResults.push({
+                id: document.id,
+                status: ImportStatus.ERROR,
+                error: e.toString(),
+              });
             }
-
-            const maxVersionNumber = await assetUtil.findMaxDocumentVersion(
-              dbDocument,
-              transactionalEntityManager,
-            );
-
-            const dbEntry = new DocumentVersion();
-            dbEntry.document = dbDocument;
-            dbEntry.text = document.text;
-            dbEntry.annotateParts = document.text
-              .split(config.application.splitRegex)
-              .filter((part) => part !== '');
-            if (maxVersionNumber !== undefined && maxVersionNumber !== null) {
-              dbEntry.version = maxVersionNumber + 1;
-            }
-
-            await transactionalEntityManager.save(DocumentVersion, dbEntry);
-            return { id: document.id, status: ImportStatus.SUCCESS };
-          } catch (e) {
-            return {
-              id: document.id,
-              status: ImportStatus.ERROR,
-              error: e.toString(),
-            };
           }
+
+          return partitionResults;
         }),
       );
+
+      // return flattened results
+      return flatten(results);
     },
   );
 
   public importQueries = this.persistenceService.wrapInTransaction(
     async (transactionalEntityManager, queries: ImportAsset[]): Promise<ImportResult[]> => {
-      return Promise.all(
-        queries.map(async (query) => {
-          try {
-            let dbQuery = await transactionalEntityManager.findOne(Query, query.id);
-            if (!dbQuery) {
-              dbQuery = new Query();
-              dbQuery.id = query.id;
+      // create partitions which are processed in parallel
+      const partitions = partitionArray(queries, NUMBER_PARALLEL_IMPORTS);
+
+      // process partitions
+      const results = await Promise.all(
+        partitions.map(async (partition) => {
+          const partitionResults: ImportResult[] = [];
+
+          for (const query of partition) {
+            try {
+              let dbQuery = await transactionalEntityManager.findOne(Query, query.id);
+              if (!dbQuery) {
+                dbQuery = new Query();
+                dbQuery.id = query.id;
+              }
+
+              const maxVersionNumber = await assetUtil.findMaxQueryVersion(
+                dbQuery,
+                transactionalEntityManager,
+              );
+
+              const dbEntry = new QueryVersion();
+              dbEntry.query = dbQuery;
+              dbEntry.text = query.text;
+              if (maxVersionNumber !== undefined && maxVersionNumber !== null) {
+                dbEntry.version = maxVersionNumber + 1;
+              }
+
+              await transactionalEntityManager.save(QueryVersion, dbEntry);
+              partitionResults.push({ id: query.id, status: ImportStatus.SUCCESS });
+            } catch (e) {
+              partitionResults.push({
+                id: query.id,
+                status: ImportStatus.ERROR,
+                error: e.toString(),
+              });
             }
-
-            const maxVersionNumber = await assetUtil.findMaxQueryVersion(
-              dbQuery,
-              transactionalEntityManager,
-            );
-
-            const dbEntry = new QueryVersion();
-            dbEntry.query = dbQuery;
-            dbEntry.text = query.text;
-            if (maxVersionNumber !== undefined && maxVersionNumber !== null) {
-              dbEntry.version = maxVersionNumber + 1;
-            }
-
-            await transactionalEntityManager.save(QueryVersion, dbEntry);
-            return { id: query.id, status: ImportStatus.SUCCESS };
-          } catch (e) {
-            return {
-              id: query.id,
-              status: ImportStatus.ERROR,
-              error: e.toString(),
-            };
           }
+
+          return partitionResults;
         }),
       );
+
+      // return flattened results
+      return flatten(results);
     },
   );
 
@@ -112,47 +141,60 @@ export class AdminService {
       // delete previous pairs
       await transactionalEntityManager.createQueryBuilder().delete().from(JudgementPair).execute();
 
-      // insert the new judgement pairs
-      return Promise.all(
-        judgementPairs.map(async (judgementPair) => {
-          try {
-            const documentPromise = transactionalEntityManager.findOne(
-              Document,
-              judgementPair.documentId,
-            );
-            const query = await transactionalEntityManager.findOne(Query, judgementPair.queryId);
-            const document = await documentPromise;
-            if (!document || !query) {
-              return {
+      // create partitions which are processed in parallel
+      const partitions = partitionArray(judgementPairs, NUMBER_PARALLEL_IMPORTS);
+
+      // process partitions
+      const results = await Promise.all(
+        partitions.map(async (partition) => {
+          const partitionResults: ImportJudgementPairResult[] = [];
+
+          for (const judgementPair of partition) {
+            try {
+              const documentPromise = transactionalEntityManager.findOne(
+                Document,
+                judgementPair.documentId,
+              );
+              const query = await transactionalEntityManager.findOne(Query, judgementPair.queryId);
+              const document = await documentPromise;
+              if (!document || !query) {
+                partitionResults.push({
+                  documentId: judgementPair.documentId,
+                  queryId: judgementPair.queryId,
+                  status: ImportStatus.ERROR,
+                  error:
+                    `either the document or the query (or both) could not be found. documentFound=${!!document}, queryFound=${!!query},` +
+                    ` documentId=${judgementPair.documentId}, queryId=${judgementPair.queryId}`,
+                });
+                continue;
+              }
+
+              const dbEntry = new JudgementPair();
+              dbEntry.document = document;
+              dbEntry.query = query;
+              dbEntry.priority = judgementPair.priority;
+              await transactionalEntityManager.save(dbEntry);
+              partitionResults.push({
+                documentId: judgementPair.documentId,
+                queryId: judgementPair.queryId,
+                status: ImportStatus.SUCCESS,
+              });
+            } catch (e) {
+              partitionResults.push({
                 documentId: judgementPair.documentId,
                 queryId: judgementPair.queryId,
                 status: ImportStatus.ERROR,
-                error:
-                  `either the document or the query (or both) could not be found. documentFound=${!!document}, queryFound=${!!query},` +
-                  ` documentId=${judgementPair.documentId}, queryId=${judgementPair.queryId}`,
-              };
+                error: e.toString(),
+              });
             }
-
-            const dbEntry = new JudgementPair();
-            dbEntry.document = document;
-            dbEntry.query = query;
-            dbEntry.priority = judgementPair.priority;
-            await transactionalEntityManager.save(dbEntry);
-            return {
-              documentId: judgementPair.documentId,
-              queryId: judgementPair.queryId,
-              status: ImportStatus.SUCCESS,
-            };
-          } catch (e) {
-            return {
-              documentId: judgementPair.documentId,
-              queryId: judgementPair.queryId,
-              status: ImportStatus.ERROR,
-              error: e.toString(),
-            };
           }
+
+          return partitionResults;
         }),
       );
+
+      // return flattened results
+      return flatten(results);
     },
   );
 
