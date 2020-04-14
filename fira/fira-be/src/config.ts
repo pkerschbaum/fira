06c2c7fa869875d796e5eb86212ceb526c854bf1
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-04-14 13:59:08 +0200

    release fira 1.4.0

--- fira/fira-be/src/config.ts
+++ fira/fira-be/src/config.ts
@@ -7,7 +7,7 @@ import NamingStrategies = require('typeorm-naming-strategies');
 const URL_REGEX = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/;
 
 export const application = {
-  version: '1.3.0',
+  version: '1.4.0',
   port: 80,
   judgementsPreloadSize: 3,
   splitRegex: /([ .\-,;]+?)/g,
