4f3d1b8fc3a8896f820deb172975ec1169bed114
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-04-21 10:37:48 +0200

    fix: log correct context

--- fira/fira-be/src/commons/logger/request-logger.ts
+++ fira/fira-be/src/commons/logger/request-logger.ts
@@ -4,40 +4,42 @@ import { RequestProperties } from '../request-properties';
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class RequestLogger implements LoggerService {
+  private context?: string;
+
   constructor(
-    private transientLogger: TransientLogger,
-    private requestProperties: RequestProperties,
+    private readonly transientLogger: TransientLogger,
+    private readonly requestProperties: RequestProperties,
   ) {}
 
   public setContext(context: string): void {
-    this.transientLogger.setContext(context);
+    this.context = context;
   }
 
   public log(message: any, context?: string): void {
     this.transientLogger.log(
       `[client-id=${this.requestProperties.getClientId()}] [request-id=${this.requestProperties.getRequestId()}] ${message}`,
-      context,
+      context ?? this.context,
     );
   }
 
   public debug(message: any, context?: string): void {
     this.transientLogger.debug(
       `[client-id=${this.requestProperties.getClientId()}] [request-id=${this.requestProperties.getRequestId()}] ${message}`,
-      context,
+      context ?? this.context,
     );
   }
 
   public warn(message: any, context?: string): void {
     this.transientLogger.warn(
       `[client-id=${this.requestProperties.getClientId()}] [request-id=${this.requestProperties.getRequestId()}] ${message}`,
-      context,
+      context ?? this.context,
     );
   }
 
   public error(message: any, context?: string): void {
     this.transientLogger.error(
       `[client-id=${this.requestProperties.getClientId()}] [request-id=${this.requestProperties.getRequestId()}] ${message}`,
-      context,
+      context ?? this.context,
     );
   }
 }
