c6c1445bcdf8a5c5d59bf5cec11307c435824a87
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-12-12 13:31:37 +0100

    fix: apply workaround to show buttons in disabled state

--- fira/fira-web/src/ui/elements/Button.tsx
+++ fira/fira-web/src/ui/elements/Button.tsx
@@ -10,18 +10,33 @@ type ButtonProps = MuiButtonProps & {
 };
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, isLoading, ...props }, ref) => (
-    <MuiButton ref={ref} disabled={isLoading} {...props}>
-      {!isLoading ? (
-        children
-      ) : (
-        <Box css={commonStyles.overlayContainer}>
-          <Box css={[commonStyles.overlayChild, commonStyles.transparent]}>{children}</Box>
-          <LoadingIndicator css={commonStyles.overlayChild} type={'secondary'} />
-        </Box>
-      )}
-    </MuiButton>
-  ),
+  ({ children, isLoading, ...props }, ref) => {
+    const disabled = props.disabled ?? isLoading;
+
+    return (
+      <MuiButton
+        ref={ref}
+        {...props}
+        disabled={disabled}
+        /* On mobile, devices simulate a hover after a click. Material-UI then displays the button
+         * with background-color "primary", even if the button is disabled (and thus, the button
+         * should be greyed out). See https://github.com/mui-org/material-ui/issues/15000.
+         * The following workaround sets the button color to "inherit" if disabled is true, then
+         * Material-UI does not set another background color on simulated hovers.
+         */
+        color={disabled ? 'inherit' : props.color}
+      >
+        {!isLoading ? (
+          children
+        ) : (
+          <Box css={commonStyles.overlayContainer}>
+            <Box css={[commonStyles.overlayChild, commonStyles.transparent]}>{children}</Box>
+            <LoadingIndicator css={commonStyles.overlayChild} type={'secondary'} />
+          </Box>
+        )}
+      </MuiButton>
+    );
+  },
 );
 
 export default Button;
